55d5ec5e5d884c40cc13ec87af212782c2702fe1
indiemellow <jeromeshaijukc2007@gmail.com>
2026-07-28 16:06:10 +0530

    made a memorydb to store curent user name and email

--- src/UI/ui/login.rs
+++ src/UI/ui/login.rs
@@ -2,29 +2,81 @@ use std::io::{self, BufRead,Write};
 use crate::account;
 use crate::storage::accountsdbms::adduser;
 use crate::storage::accountsdbms::userinfofomdb;
+use crate::storage::accountsdbms::getemails;
+use crate::storage::accountsdbms::password_check;
+use crate::storage::accountsdbms::current_user;
+use crate::storage::accountsdbms::get_name;
 
 
 
 
 pub fn login_interface(){
-    println!("Email address: ");
-            let mut email = String::new();
-            io::stdin().read_line(&mut email).expect("Failed to read line");
-            println!("Password: ");
-            let mut password = String::new();
-            io::stdin().read_line(&mut password).expect("Failed to read line");
-            println!("username: ");
-            let mut username = String::new();
-            io::stdin().read_line(&mut username).expect("Failed to read line");
+    println!("=======================L=O=G=I=N=======================");
+    println!("choose one of the following options:");
+    let emails =getemails().expect("Failed to get emails from database");
+    if emails.is_empty() {
+        println!("No users found in the database. Please create a new account.");
+    }
+    else {
+        for (i, email) in emails.iter().enumerate() {
+            println!("{}. {}", i + 1, email);
+        }
+    }
+    println!("enter 0 to create a new account or select an existing account by number:");
+    println!("enter exit to exit the login interface.");
+    loop {
+        print!("-> ");
+        io::stdout().flush().expect("Failed to flush stdout");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        println!("you have requested:{}",input);
+        let command = input.trim();
+        match command {
+            "exit" => {
+                println!("exit the login interface.");
+                break;
+            }
+            "0" => {
+                println!("Email address: ");
+                let mut email = String::new();
+                io::stdin().read_line(&mut email).expect("Failed to read line");
+                println!("Password: ");
+                let mut password = String::new();
+                io::stdin().read_line(&mut password).expect("Failed to read line");
+                println!("username: ");
+                let mut username = String::new();
+                io::stdin().read_line(&mut username).expect("Failed to read line");
 
-            let email = email.trim();
-            let password = password.trim();
-            let username = username.trim();
+                let email = email.trim();
+                let password = password.trim();
+                let username = username.trim();
 
-            println!("You entered email: {}, password: {}, username: {}", email, password, username);
-            let account = account::Account::new(email.to_string(),username.to_string(), password.to_string());
-            adduser(&account.email, &account.name, &account.password).expect("Failed to add user");
-
+                println!("You entered email: {}, password: {}, username: {}", email, password, username);
+                let account = account::Account::new(email.to_string(), username.to_string(), password.to_string());
+                adduser(&account.email, &account.name, &account.password).expect("Failed to add user");
+                current_user(account.email, account.name).expect("Failed to set current user");
+                println!("Account created successfully!");
+                break
+            }
+            _ => {
+                println!("you have selected the account: {}",emails.get(command.parse::<usize>().unwrap_or(0) - 1).unwrap_or(&"Invalid selection".to_string()));
+                println!("please enter the password for the selected account:");
+                print!("-> ");
+                io::stdout().flush().expect("Failed to flush stdout");
+                let mut password = String::new();
+                io::stdin().read_line(&mut password).expect("Failed to read line");
+                let password = password.trim();
+                if password_check(emails.get(command.parse::<usize>().unwrap_or(0) - 1).unwrap_or(&"Invalid selection".to_string()), password).unwrap_or(false){
+                    println!("you have logged in with the account: {}",emails.get(command.parse::<usize>().unwrap_or(0) - 1).unwrap_or(&"Invalid selection".to_string()));
+                }else{
+                    println!("Incorrect password for the account: {}",emails.get(command.parse::<usize>().unwrap_or(0) - 1).unwrap_or(&"Invalid selection".to_string()));
+                }
+                let email = emails.get(command.parse::<usize>().unwrap_or(0) - 1).unwrap_or(&"Invalid selection".to_string()).to_string();
+                current_user(email.clone(), get_name(email).unwrap_or_else(|_e| "Unknown".to_string())).expect("Failed to set current user");
+                break
+            }
+        }
+    }
 }
 pub fn userinfo(){
     userinfofomdb();
--- src/storage/accountsdbms.rs
+++ src/storage/accountsdbms.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 use rusqlite::{params, Connection, Result};
 
 #[derive(Debug)]
@@ -6,6 +8,11 @@ struct user {
     email: String,
     name: String,
     password: String,
+}
+struct current_user {
+    id: i32,
+    email: String,
+    name: String,
 }
 
 pub fn accountsdb_creation() -> Result<()> {
@@ -27,7 +34,7 @@ pub fn accountsdb_creation() -> Result<()> {
         password: "root".to_string(),
     };
     conn.execute(
-        "INSERT INTO user (email, name, password) VALUES (?1, ?2, ?3)",
+        "INSERT OR IGNORE INTO user(email, name, password) VALUES (?1, ?2, ?3)",
         (&me.email, &me.name, &me.password),
     )?;
 
@@ -50,7 +57,7 @@ pub fn accountsdb_creation() -> Result<()> {
 pub fn adduser(email: &str, name: &str, password: &str) -> Result<()> {
     let conn = Connection::open("accounts.db")?;
     conn.execute(
-        "INSERT INTO user (email, name, password) VALUES (?1, ?2, ?3)",
+        "INSERT OR IGNOREINTO user (email, name, password) VALUES (?1, ?2, ?3)",
         params![email, name, password],
     )?;
     println!("User added to database");
@@ -76,4 +83,67 @@ pub fn userinfofomdb() -> Result<()> {
             println!("User info: {:?}", user?);
         }
     Ok(())
+}
+
+pub fn getemails()->Result<Vec<String>>{
+ let conn = Connection::open("accounts.db")?;
+
+
+    let mut stmt = conn.prepare("SELECT id, email, name, password FROM user")?;
+    let user_iter = stmt.query_map([], |row| {
+        Ok(user {
+            id: row.get(0)?,
+            email: row.get(1)?,
+            name: row.get(2)?,
+            password: row.get(3)?,
+        })
+    })?;
+    let mut emails = Vec::new();
+    for user in user_iter {
+        emails.push(user?.email.to_string());
+    }
+    Ok(emails)
+}
+pub fn password_check(email: &str, password: &str) -> Result<bool> {
+    let conn = Connection::open("accounts.db")?;
+
+    let mut stmt = conn.prepare("SELECT password FROM user WHERE email = ?1")?;
+    let mut rows = stmt.query(params![email])?;
+
+    if let Some(row) = rows.next()? {
+        let stored_password: String = row.get(0)?;
+        Ok(stored_password == password)
+    } else {
+        Ok(false) // Email not found
+    }
+}
+pub fn get_name(email: String) -> Result<String> {
+    let conn = Connection::open("accounts.db")?;
+
+    let mut stmt = conn.prepare("SELECT name FROM user WHERE email = ?1")?;
+    let mut rows = stmt.query(params![email])?;
+    let name: String = rows.next()?.unwrap().get(0)?;
+    Ok(name)
+}
+pub fn current_user(email: String,name: String)-> Result<()>{
+    let conn = Connection::open_in_memory()?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS current_user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL,
+            name TEXT NOT NULL
+        )",
+        [],
+    )?;
+    let me = current_user{
+        id: 0,
+        email: email.to_string(),
+        name: name.to_string(),
+    };
+    conn.execute(
+        "INSERT OR IGNORE INTO current_user (email, name) VALUES (?1, ?2)",
+        (&me.email, &me.name),
+    )?;
+    Ok(())
+
 }
